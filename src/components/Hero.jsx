b5da67c6b83ddbe262af3a94434a3e7c448dba32
niravgohel74 <niravgohel2000@gmail.com>
2024-10-19 14:41:03 +0530

    Note line added and minor changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -46,8 +46,13 @@ const Hero = () => {
 
   return (
     <section className="w-full nav-height bg-black relative">
-      <div className="h-5/6 w-full flex-center flex-col">
-        <p id="hero" className="hero-title">iPhone 15 Pro</p>
+      <div className="h-5/6 w-full flex-center flex-col mt-6">
+        <p
+          id="hero"
+          className="hero-title relative"
+        >
+          iPhone 15 Pro
+        </p>
         <div className="md:w-10/12 w-9/12">
           <video
             className="pointer-events-none"
@@ -62,8 +67,13 @@ const Hero = () => {
           </video>
         </div>
       </div>
-      <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
-        <a href="#highlights" className="btn">Buy</a>
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
         <p className="font-normal text-xl">From @199/month or @999</p>
       </div>
     </section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,72 @@
-import { appleImg, bagImg, searchImg } from "../utils"
-import { navLists } from "../constants"
-
+import { useEffect, useRef } from "react";
+import { appleImg, bagImg, searchImg } from "../utils";
+import { navLists } from "../constants";
+import gsap from "gsap";
 
 const Navbar = () => {
+    const footnoteRef = useRef(null);
+  
+    useEffect(() => {
+      if (footnoteRef.current) {
+        gsap.fromTo(footnoteRef.current, {
+          opacity: 0, 
+        }, {
+          opacity: 1,
+          duration: 1.5,
+          ease: "power3.out",
+        });
+      }
+    }, []);
+
   return (
-    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+    <>
+      <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
         <nav className="flex w-full screen-max-width">
-            <img src={ appleImg } alt="apple" width={14} height={16} />
+          <img src={appleImg} alt="apple" width={14} height={16} />
 
-            <div className="flex flex-1 justify-center">
-                {navLists.map((nav, i) => (
-                    <div key={nav} className="px-5  cursor-pointer text-gray hover:text-white transition-all">
-                        {nav}
-                    </div>
-                ))}
-            </div>
-            <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
-                <img src={searchImg} alt="search" width={18} height={18} />
-                <img src={bagImg} alt="bag" width={18} height={18} />
-            </div>
+          <div className="flex flex-1 justify-center">
+            {navLists.map((nav, i) => (
+              <div
+                key={nav}
+                className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all"
+              >
+                {nav}
+              </div>
+            ))}
+          </div>
+          <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+            <img src={searchImg} alt="search" width={14} height={16} />
+            <img src={bagImg} alt="bag" width={14} height={16} />
+          </div>
         </nav>
-    </header>
-  )
-}
+      </header>
 
-export default Navbar
+      <div
+        id="footnote"
+        ref={footnoteRef}
+        className="bg-white bg-opacity-10 p-2 w-full text-center text-sm opacity-0 flex items-center justify-center" 
+      >
+        <span>
+          $650 in credit toward iPhone&nbsp;16 or iPhone&nbsp;16&nbsp;Pro when
+          you trade in iPhone&nbsp;12 or&nbsp;higher.
+        </span>
+        <span className="footnote footnote-supglghy">
+          <a href="#" aria-label="Footnote * symbol" data-model-close>
+            *
+          </a>
+        </span>
+        <a
+          className="text-[#2997ff] hover:underline flex items-center ml-1"
+          href="#highlights"
+          data-analytics-title="buy | iphone 16 pro"
+          aria-label="Buy iPhone 16 Pro"
+        >
+          Buy
+          <span className="ml-1 text-lg">›</span>
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
